417a194f20c63e850da1cd03a350eb1ad8cb19c3
Ayesha <ayeshasultanarima@gmail.com>
2018-05-15 18:42:39 -0400

    HackerRank 30-days-of Code Day1: Data Types
    Task 
    Complete the code in the editor below. The variables  i, d, and s are already declared and initialized for you. You must:
    
    Declare  variables: one of type int, one of type double, and one of type String.
    Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
    Use the  + operator to perform the following operations: 
    Print the sum of i  plus your int variable on a new line.
    Print the sum of d plus your double variable to a scale of one decimal place on a new line.
    Concatenate s  with the string you read as input and print the result on a new line.
    Note: If you are using a language that doesn't support using  for string concatenation (e.g.: C), you can just print one variable immediately following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.
    
    Input Format
    
    The first line contains an integer that you must sum with i. 
    The second line contains a double that you must sum with d. 
    The third line contains a string that you must concatenate with s.
    
    Output Format
    
    Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
    
    Sample Input
    
    12
    4.0
    is the best place to learn and practice coding!
    Sample Output
    
    16
    8.0
    HackerRank is the best place to learn and practice coding!
    Explanation
    
    When we sum the integers  4 and 12 , we get the integer 16. 
    When we sum the floating-point numbers 4.0 and  4.0, we get 8.0. 
    When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
    
    You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading input from stdin.

---
+++ DataTypes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomainp>
+#include<limits>
+using namespace std;
+
+int main(){
+
+// Declare second integer, double, and String variables.
+int i2;
+double d2;
+string s2 ="is the best place to learn and practice coding!";
+
+// Read and save an integer, double, and String to your variables.
+cin >> i2;
+cin >> d2;
+cin.ignore(); //write it before getline gets executed otherwise it will cause issues while getting numeric and string values from regular cin>>i2>>d2 to getline(cin,s2)
+getline(cin, s2); //using getline(cin, s2) will get the full string length(the entire line) otherwise it won't execute the whole line
+
+// Print the sum of both integer variables on a new line.
+cout << i + i2 << endl;
+
+// Print the sum of the double variables on a new line.
+cout << fixed << setprecision(1) << d + d2 << endl; //fixed setprecision(1) will get the decimal point after 4
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+cout << s + s2 << endl;
+return 0;
+}
